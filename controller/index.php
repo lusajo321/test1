2683ecd4926d45253c77b2f78a2ca1c9ae7a7563
Alfred Kalinga <alfredkalinga99@gmail.com>
2026-08-16 16:05:27 +0300

    New release

---
+++ controller/index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace controller;
+
+class Admin_IndexController extends \controller
+{
+    public function init()
+    {
+        
+    }
+
+    public function indexAction()
+    {
+        
+    }
+}
+?>
